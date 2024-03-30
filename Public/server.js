cef3ce1e7e878b87af6e2daaaf6b3f14fcb2806b
moneygoyal02 <goyalmoney95@gmail.com>
2024-03-30 16:09:46 +0530

    customer profile

--- Public/server.js
+++ Public/server.js
@@ -14,7 +14,7 @@ const congObj = {
   user: "root",
   password: "Money@90410",
   database: "devcre",
- 
+  dateStrings: true,
 };
 
 const mysql = mysql2.createConnection(congObj);
@@ -84,9 +84,136 @@ app.get("/", function (req, resp) {
       }
     );
   });
-
+//-----------customer dash-------------------------
   app.get("/cd", function (req, resp) {
     let filePath = process.cwd() + "/web/customer-Dash.html";
     resp.sendFile(filePath);
   });
+
+  app.get("/changepass", function (req, resp) {
+    let email = req.query.email;
+    let oldpass = req.query.oldpwd;
+    let newpass = req.query.newpwd;
+  
+    mysql.query(
+      "update users set pwd = ? WHERE emailid = ? and pwd = ?",
+      [newpass, email, oldpass],
+      function (err) {
+        if (err) {
+          resp.send("Error updating password");
+        } else resp.send("Update successful");
+      }
+    );
+  });
+ 
+  // ----------------------------------------------------------
+  app.get("/cp", function (req, resp) {
+    let filePath = process.cwd() + "/web/customer-profile.html";
+    resp.sendFile(filePath);
+  });
+
+  app.post("/saveee", function (req, resp) {
+    
+  
+    if (req.body.checkbox === "checked") {
+      const namee = req.body.name;
+      const contact = req.body.contact;
+      const addr = req.body.address;
+      const state = req.body.state;
+      const city = req.body.city;
+  
+      const email = req.body.email;
+  
+      console.log(email);
+  
+      let filename;
+      if (req.files == null) filename = "nopic.jpg";
+      else {
+        filename = req.files.ppic.name;
+        let path = process.cwd() + "/web/pics/uploads/" + filename;
+        req.files.ppic.mv(path);
+      }
+  
+      req.body.ppic = filename;
+  
+      mysql.query(
+        " insert into cuprofile values(?,?,?,?,?,?,?)",
+        [email, namee, contact, addr, city, state, filename],
+        function (err) {
+          if (err == null) {
+            console.log("Profile saved Successfully");
+  
+            resp.send("Profile saved Successfully");
+          } else {
+            console.log(err.message);
+            resp.send("Error saving profile");
+          }
+        }
+      );
+    } else {
+      console.log("please aggre to term&conditions");
+      resp.send("Please agree to terms & conditions");
+    }
+  });
+  
+  app.get("/search", function (req, resp) {
+    const email = req.query.email;
+  
+    mysql.query(
+      "select * from cuprofile where emailid=?",
+      [email],
+      function (err, ary) {
+        console.log(ary);
+        resp.send(ary);
+      }
+    );
+  });
+  
+  app.post("/update", function (req, resp) {
+   
+  
+    if (req.body.checkbox === "checked") {
+      const namee = req.body.name;
+      const contact = req.body.contact;
+      const addr = req.body.address;
+      const state = req.body.state;
+      const city = req.body.city;
+  
+      const email = req.body.email;
+  
+      let filename;
+      if (req.files && req.files.ppic) {
+        // Check if req.files is not null or undefined, and if ppic property exists
+        filename = req.files.ppic.name;
+        let path = process.cwd() + "/web/pics/uploads/" + filename;
+        req.files.ppic.mv(path);
+      } else {
+        // Handle case when req.files is null or undefined
+        filename = req.body.hdn;
+      }
+      req.body.ppic = filename;
+  
+      
+  
+      mysql.query(
+        " update cuprofile set Fname=? , contact=? , address=? , city=? , state=? , ppic=? where emailid=? ",
+        [namee, contact, addr, city, state, filename, email],
+        function (err) {
+          if (err == null) {
+            console.log("Profile updated Successfully");
+  
+            resp.send("Profile updated Successfully");
+          } else {
+            console.log(err.message);
+            resp.send("Error updating profile");
+          }
+        }
+      );
+    } else {
+      console.log("please aggre to term&conditions");
+      resp.send("Please agree to terms & conditions");
+    }
+  });
+
+  
 
