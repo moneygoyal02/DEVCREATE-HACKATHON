c6d78096b24306a117bce7e3637263fb6dc5d471
moneygoyal02 <goyalmoney95@gmail.com>
2024-03-30 22:06:41 +0530

    ..

--- Public/database.sql
+++ Public/database.sql
@@ -4,3 +4,7 @@ use devcre;
 create table users (emailid varchar(30) primary key, pwd varchar(30) , usertype varchar(40) , dos date , status int );
 
 select * from users;
+
+create table cuprofile (emailid varchar(30) primary key , FName varchar(30) ,  contact varchar(15) , address varchar(100) ,  city varchar(30) , state varchar(50) , ppic varchar(300) );
+
+select * from cuprofile; 
